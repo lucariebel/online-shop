b1be7fb4c076e18690646872858aca1edc1ad1f2
lucariebel <156175235+lucariebel@users.noreply.github.com>
2024-11-05 13:35:29 +0100

    WSP-23: added article controller & service with http methods

---
+++ root/backend-root/Backend/Controllers/ArticleController.cs
@@ -0,0 +1,42 @@
+using Backend.Classes;
+using Backend.Context;
+using Backend.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        private readonly IArticleService _articleService;
+
+        public ArticleController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+        
+
+        // Get: api/Article
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DirectBuyArticle>>> GetArticles()
+        {
+            return await _articleService.GetArticles();
+        }
+
+        // Get: api/Article/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DirectBuyArticle>> GetArticle(int id)
+        {
+            return await _articleService.GetArticle(id);
+        }
+
+        // Post: api/Article
+        [HttpPost]
+        public async Task<ActionResult<DirectBuyArticle>> PostArticle(DirectBuyArticle directBuyArticle)
+        {
+            return await _articleService.PostArticle(directBuyArticle);
+        }
+    }
+}
---
+++ root/backend-root/Backend/Services/ArticleService.cs
@@ -0,0 +1,50 @@
+﻿using Backend.Classes;
+using Backend.Context;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Services
+{
+    public partial interface IArticleService
+    {
+        Task<ActionResult<IEnumerable<DirectBuyArticle>>> GetArticles();
+        Task<ActionResult<DirectBuyArticle>> GetArticle(int id);
+        Task<ActionResult<DirectBuyArticle>> PostArticle(DirectBuyArticle directBuyArticle);
+    }
+    public partial class ArticleService : ControllerBase, IArticleService
+    {
+        private readonly WebShop24DbContext _context;
+
+        public ArticleService(WebShop24DbContext context)
+        {
+            _context = context;
+        }
+
+        // Get
+        public async Task<ActionResult<IEnumerable<DirectBuyArticle>>> GetArticles()
+        {
+            return await _context.Articles.ToListAsync();
+        }
+
+        public async Task<ActionResult<DirectBuyArticle>> GetArticle(int id)
+        {
+            DirectBuyArticle directBuyArticle = await _context.Articles.FindAsync(id);
+
+            if(directBuyArticle == null)
+            {
+                return NotFound();
+            }
+            return directBuyArticle;
+        }
+
+        // Post
+        public async Task<ActionResult<DirectBuyArticle>> PostArticle(DirectBuyArticle directBuyArticle)
+        {
+            _context.Articles.Add(directBuyArticle);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetArticle", new { id = directBuyArticle.ArticleId }, directBuyArticle);
+        }
+    }
+}
