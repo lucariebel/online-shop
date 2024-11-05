b06cc99d8a4c1810cc29df2a61e2134fbe8513bc
lucariebel <156175235+lucariebel@users.noreply.github.com>
2024-11-05 17:33:36 +0100

    WSP-27: added simple http get to get data from backend

--- root/frontend-root/src/app/app.component.ts
+++ root/frontend-root/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ArticleService } from './components/core/services/article.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'frontend-root';
+
+  constructor(public articleService: ArticleService) {}
+
+  async getArticles(): Promise<any> {
+    await this.articleService.getArticles();
+  }
 }
