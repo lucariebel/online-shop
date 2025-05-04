7cd89a6ec1b27a3d09a1e125a286fe13f2c8f198
Sebastian Albert <albert_sebastian@gmx.net>
2025-05-04 20:14:00 +0200

    WSP-48: fertig

--- root/frontend-root/src/app/components/auction-list/auction-list.component.ts
+++ root/frontend-root/src/app/components/auction-list/auction-list.component.ts
@@ -13,6 +13,7 @@ import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
 import { AuctionService } from '../../core/services/auction.service';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auction-list',
@@ -29,6 +30,7 @@ import { RouterLink } from '@angular/router';
     MatCardSubtitle,
     EurFormatPipe,
     RouterLink,
+    CommonModule,
   ],
   templateUrl: './auction-list.component.html',
 })
