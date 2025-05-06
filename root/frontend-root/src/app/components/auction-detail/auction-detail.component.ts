e0cb7038bc6b559ea0b7168f7f62fe1a8c928c4f
stefanschfr <sfschaefer@web.de>
2025-05-06 22:06:43 +0200

    WSP-12: add bid to frontend

--- root/frontend-root/src/app/components/auction-detail/auction-detail.component.ts
+++ root/frontend-root/src/app/components/auction-detail/auction-detail.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AuctionArticle } from '../../core/interfaces/AuctionArticle';
 import { ActivatedRoute } from '@angular/router';
 import { AuctionService } from '../../core/services/auction.service';
 import { EurFormatPipe } from '../../core/pipes/eur-format.pipe';
-import { NgForOf } from '@angular/common';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgForOf } from '@angular/common';
+import { AuctionBidDialogComponent } from '../auction-bid-dialog/auction-bid-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { UserService } from '../../core/services/user.service';
 
 @Component({
   selector: 'app-auction-detail',
@@ -13,10 +15,12 @@ import { CommonModule } from '@angular/common';
   templateUrl: './auction-detail.component.html',
 })
 export class AuctionDetailComponent implements OnInit {
+  readonly dialog = inject(MatDialog);
   auction!: AuctionArticle | undefined;
 
   constructor(
     public auctionService: AuctionService,
+    public userService: UserService,
     private route: ActivatedRoute,
   ) {}
 
@@ -30,5 +34,30 @@ export class AuctionDetailComponent implements OnInit {
       console.error(`Artikel mit ID ${id} nicht gefunden!`);
       // this.router.navigate(['/not-found']);
     }
+  }
+
+  openDialog() {
+    const currentBid = this.auction?.bid ?? 0;
+
+    const dialogRef = this.dialog.open(AuctionBidDialogComponent, {
+      data: { bid: currentBid },
+    });
+
+    dialogRef.afterClosed().subscribe(async (result) => {
+      if (result != null && this.auction) {
+        const updatedAuction = {
+          ...this.auction,
+          bid: result,
+          winnerId: this.userService.user.userId,
+        };
+        console.log(updatedAuction);
+        try {
+          await this.auctionService.putAuction(updatedAuction);
+          this.auction.bid = result;
+        } catch (error) {
+          console.error('Fehler beim Aktualisieren der Auktion:', error);
+        }
+      }
+    });
   }
 }
