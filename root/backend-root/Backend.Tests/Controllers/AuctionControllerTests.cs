cc4065aea9e53a209f2e9381312fa7e757458d03
Luca Riebel <luca.riebel.1104@gmail.com>
2025-07-06 14:20:33 +0200

    WSP-46: tests for Controllers

---
+++ root/backend-root/Backend.Tests/Controllers/AuctionControllerTests.cs
@@ -0,0 +1,117 @@
+﻿using Backend.Classes;
+using Backend.Controllers;
+using Backend.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Backend.Tests.Controllers
+{
+    [TestFixture]
+    public class AuctionControllerTests
+    {
+        private Mock<IAuctionService> _mockService;
+        private AuctionController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockService = new Mock<IAuctionService>();
+            _controller = new AuctionController(_mockService.Object);
+        }
+
+        [Test]
+        public async Task GetAuctions_ShouldReturnAllAuctions()
+        {
+            // Arrange
+            var auctions = new List<AuctionArticle>
+            {
+                new() { ArticleId = 1, ArticleName = "Auktion 1" },
+                new() { ArticleId = 2, ArticleName = "Auktion 2" }
+            };
+            _mockService.Setup(s => s.GetAuctions()).ReturnsAsync(auctions);
+
+            // Act
+            var result = await _controller.GetAuctions();
+
+            // Assert
+            Assert.That(result.Value, Is.EqualTo(auctions));
+        }
+
+        [Test]
+        public async Task GetAuction_ShouldReturnAuctionById()
+        {
+            // Arrange
+            var auction = new AuctionArticle { ArticleId = 5, ArticleName = "Einzelauktion" };
+            _mockService.Setup(s => s.GetAuction(5)).ReturnsAsync(auction);
+
+            // Act
+            var result = await _controller.GetAuction(5);
+
+            // Assert
+            Assert.That(result.Value, Is.Not.Null);
+            Assert.That(result.Value.ArticleId, Is.EqualTo(5));
+        }
+
+        [Test]
+        public async Task GetRandomAuctions_ShouldReturnCount()
+        {
+            // Arrange
+            var auctions = new List<AuctionArticle>
+            {
+                new() { ArticleId = 10 },
+                new() { ArticleId = 11 }
+            };
+            _mockService.Setup(s => s.GetRandomArticles(2)).ReturnsAsync(auctions);
+
+            // Act
+            var result = await _controller.GetRandomAuctions(2);
+
+            // Assert
+            Assert.That(result.Value.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public async Task PostAuction_ShouldReturnCreatedAuction()
+        {
+            // Arrange
+            var newAuction = new AuctionArticle { ArticleId = 7, ArticleName = "Neue Auktion" };
+            _mockService.Setup(s => s.PostAuction(newAuction)).ReturnsAsync(newAuction);
+
+            // Act
+            var result = await _controller.PostAuction(newAuction);
+
+            // Assert
+            Assert.That(result.Value, Is.EqualTo(newAuction));
+        }
+
+        [Test]
+        public async Task PutArticle_ShouldReturnNoContent()
+        {
+            // Arrange
+            var auction = new AuctionArticle { ArticleId = 3 };
+            _mockService.Setup(s => s.PutAuction(3, auction)).ReturnsAsync(new NoContentResult());
+
+            // Act
+            var result = await _controller.PutArticle(3, auction);
+
+            // Assert
+            Assert.IsInstanceOf<NoContentResult>(result);
+        }
+
+        [Test]
+        public async Task DeleteAuction_ShouldReturnNoContent()
+        {
+            // Arrange
+            _mockService.Setup(s => s.DeleteAuction(9)).ReturnsAsync(new NoContentResult());
+
+            // Act
+            var result = await _controller.DeleteAuction(9);
+
+            // Assert
+            Assert.IsInstanceOf<NoContentResult>(result);
+        }
+    }
+}
---
+++ root/backend-root/Backend.Tests/Controllers/SearchControllerTests.cs
@@ -0,0 +1,95 @@
+﻿using Backend.Classes;
+using Backend.Controllers;
+using Backend.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Backend.Tests.Controllers
+{
+    [TestFixture]
+    public class SearchControllerTests
+    {
+        private Mock<ISearchService> _mockSearchService;
+        private SearchController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockSearchService = new Mock<ISearchService>();
+            _controller = new SearchController(_mockSearchService.Object);
+        }
+
+        [Test]
+        public void SearchDirectBuyArticle_ShouldReturnArticles_WhenFound()
+        {
+            // Arrange
+            var searchTerm = "Laptop";
+            var resultList = new List<DirectBuyArticle>
+            {
+                new() { ArticleId = 1, ArticleName = "Gaming Laptop" }
+            };
+
+            _mockSearchService.Setup(s => s.SearchDirectBuyArticle(searchTerm)).Returns(resultList);
+
+            // Act
+            var result = _controller.SearchDirectBuyArticle(searchTerm);
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            var okResult = result.Result as OkObjectResult;
+            Assert.That(okResult.Value, Is.EqualTo(resultList));
+        }
+
+        [Test]
+        public void SearchDirectBuyArticle_ShouldReturnBadRequest_OnException()
+        {
+            // Arrange
+            var searchTerm = "Laptop";
+            _mockSearchService.Setup(s => s.SearchDirectBuyArticle(searchTerm)).Throws(new Exception("Fehler"));
+
+            // Act
+            var result = _controller.SearchDirectBuyArticle(searchTerm);
+
+            // Assert
+            Assert.IsInstanceOf<BadRequestObjectResult>(result.Result);
+        }
+
+        [Test]
+        public void SearchAuctionArticle_ShouldReturnArticles_WhenFound()
+        {
+            // Arrange
+            var searchTerm = "Buch";
+            var resultList = new List<DirectBuyArticle>
+            {
+                new() { ArticleId = 2, ArticleName = "Harry Potter Auktion" }
+            };
+
+            _mockSearchService.Setup(s => s.SearchDirectBuyArticle(searchTerm)).Returns(resultList);
+
+            // Act
+            var result = _controller.SearchDirectBuyArticle(searchTerm);
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            var okResult = result.Result as OkObjectResult;
+            Assert.That(okResult.Value, Is.EqualTo(resultList));
+        }
+
+        [Test]
+        public void SearchAuctionArticle_ShouldReturnBadRequest_OnException()
+        {
+            // Arrange
+            var searchTerm = "Buch";
+            _mockSearchService.Setup(s => s.SearchAuctionArticle(searchTerm)).Throws(new Exception("Fehler"));
+
+            // Act
+            var result = _controller.SearchAuctionArticle(searchTerm);
+
+            // Assert
+            Assert.IsInstanceOf<BadRequestObjectResult>(result.Result);
+        }
+    }
+}
